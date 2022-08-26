80016d89d6096e4d4463ff5cefb034d8ea71c650
seongyeon <ysy2565@naver.com>
2022-08-26 21:36:34 +0900

    setting: react-router-dom 설치

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
 );
