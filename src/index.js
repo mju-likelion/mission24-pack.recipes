03e8e70b9e083de4c155048802dd89ba316dbace
yuraup <o721@naver.com>
2022-08-27 19:28:46 +0900

    setting: recoil 설치

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <RecoilRoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecoilRoot>,
 );
