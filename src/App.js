fe9f574a7f85940916ce0666bebbc6c346329a37
seongyeon <ysy2565@naver.com>
2022-08-28 06:48:55 +0900

    feat: develop merge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
 import 'normalize.css';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/theme';
-import Modal from './components/Modal';
-import { useState } from 'react';
 import List from './components/List';
 
 function App() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const modalClose = () => {
-    setModalOpen(!modalOpen);
-  };
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Header />
       <List />
-      <button onClick={modalClose}>모달 테스트</button>
-      {modalOpen && <Modal modalClose={modalClose}></Modal>}
     </ThemeProvider>
   );
 }
