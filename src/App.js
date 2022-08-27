a620c931b298010dd080157a4d0f3ea5ca550f70
olivyou2 <olivyou2@gmail.com>
2022-08-28 05:38:17 +0900

    feat: footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import GlobalStyle from './styles/GlobalStyles';
 import 'normalize.css';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/theme';
+import Footer from './component/footer';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Footer />
     </ThemeProvider>
   );
 }
---
+++ src/component/footer.js
@@ -0,0 +1,63 @@
+import styled from 'styled-components';
+
+import { ReactComponent as GithubIcon } from '../images/github.svg';
+import { ReactComponent as InstagramIcon } from '../images/instagram.svg';
+
+function Footer() {
+  return (
+    <>
+      <FooterElement>
+        <FooterEmail>mju@likelion.org</FooterEmail>
+        <FooterIcons>
+          <GithubStyledIcon />
+          <VerticalLine />
+          <InstagramStyledIcon />
+        </FooterIcons>
+      </FooterElement>
+    </>
+  );
+}
+
+const FooterElement = styled.div`
+  width: 100vw;
+  height: 80px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  position: fixed;
+  bottom: 0px;
+`;
+
+const FooterEmail = styled.div`
+  margin-left: 46px;
+  letter-spacing: 1px;
+`;
+
+const FooterIcons = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-right: 60px;
+  align-items: center;
+`;
+
+const VerticalLine = styled.div`
+  height: 20px;
+  border-left: 2px solid #424242;
+`;
+
+const GithubStyledIcon = styled(GithubIcon)`
+  background-color: #424242;
+  margin-right: 19px;
+  width: 24px;
+  height: 24px;
+`;
+
+const InstagramStyledIcon = styled(InstagramIcon)`
+  margin-left: 19px;
+  width: 24px;
+  height: 24px;
+`;
+
+export default Footer;
