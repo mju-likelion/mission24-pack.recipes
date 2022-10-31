5ef564b4a384c39fd8d6e1bf2a9ed9074c6f8c8d
ChoiWonYu <phillip07290@naver.com>
2022-10-31 20:12:26 +0900

    feat: save refreshToken, accessToken

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { ReactComponent as Logo } from '../images/logo.svg';
 
 const Header = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) {
+      setIsLogin(true);
+    }
+  }, []);
   return (
-    <>
-      <HeaderWrapper>
-        <Link to={'/'}>
-          <LogoBox />
-        </Link>
-        <LinkBox>
-          <Link to={'/login'}>
-            <Button>로그인</Button>
-          </Link>
-          <p>|</p>
-          <Link to={'/register'}>
-            <Button>회원가입</Button>
-          </Link>
-        </LinkBox>
-      </HeaderWrapper>
-    </>
+    <HeaderWrapper>
+      <Link to={'/'}>
+        <LogoBox />
+      </Link>
+      <LinkBox>
+        {isLogin ? (
+          <>
+            <Button>로그아웃</Button>
+          </>
+        ) : (
+          <>
+            <Link to={'/login'}>
+              <Button>로그인</Button>
+            </Link>
+            <p>|</p>
+            <Link to={'/register'}>
+              <Button>회원가입</Button>
+            </Link>
+          </>
+        )}
+      </LinkBox>
+    </HeaderWrapper>
   );
 };
 
