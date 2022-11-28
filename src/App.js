b41284addc84c8b66ca9a67ee879399970915fe9
seongyeon <ysy2565@naver.com>
2022-11-28 13:26:52 +0900

    feat: 에러페이지 이름 변경

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import RegisterPage from './page/Register';
 // import Toast from './components/Toast';
 import Footer from './components/Footer';
 import ListBox from './components/List';
-import Error from './components/Error';
+import NotFound from './components/NotFound';
 
 import { Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -57,7 +57,7 @@ function App() {
               </>
             }
           />
-          <Route path='/*' element={<Error />} />
+          <Route path='/*' element={<NotFound />} />
           <Route
             path='/register'
             element={
