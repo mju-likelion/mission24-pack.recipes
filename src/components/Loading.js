600d5d3a5c6fe555935023f76ae68c585e162451
ChoiWonYu <phillip07290@naver.com>
2023-01-27 13:24:30 +0900

    feat: useEffect clear function

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,10 +5,14 @@ import { ReactComponent as GreenApple } from '../images/greenApple.svg';
 const Loading = () => {
   const [isRotated, setIsRotated] = useState(false);
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setIsRotated(!isRotated);
     }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [isRotated]);
+
   return (
     <Backgroud>
       <LoadingContainer>
@@ -32,17 +36,20 @@ const Backgroud = styled.div`
   justify-content: center;
   align-items: center;
 `;
+
 const LoadingContainer = styled.div`
   display: flex;
   flex-direction: column;
   text-align: center;
 `;
+
 const StyledGreenApple = styled(GreenApple)`
   transform: ${(props) => (props.isrotated ? 'rotate(0)' : 'rotate(30deg)')};
   transition: transform 1000ms ease;
   width: 97px;
   height: 54px;
 `;
+
 const StyledText = styled.div`
   font-size: 30px;
   font-weight: 400;
