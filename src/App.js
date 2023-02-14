06d1e5871b600e0aa7980d9d4d704b4dc5a3b7b8
ChoiWonYu <phillip07290@naver.com>
2023-02-14 20:29:23 +0900

    style: toast message style

--- src/App.js
+++ src/App.js
@@ -83,10 +83,17 @@ const StyledToastContainer = styled(ToastContainer)`
   .Toastify__toast {
     box-shadow: none;
     background-color: ${({ theme }) => theme.colors.green};
+    color: ${({ theme }) => theme.colors.black};
     border-radius: 20px;
-    height: 100px;
+    height: 150px;
     width: 392px;
+    font-size: 25px;
     line-height: 30px;
+    @media screen and (max-width: 599px) and (min-width: 375px) {
+      font-size: 18px;
+      width: 250px;
+      height: 100px;
+    }
   }
 `;
 
